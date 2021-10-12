5c807043c23b296f18fc1bca9b39c0ad2438659c
Issac <lim_527@naver.com>
2021-10-12 20:55:41 +0900

    10-12  - ProjectApp 작업 완료

--- projectapp/models.py
+++ projectapp/models.py
@@ -6,9 +6,13 @@ LANGUAGE_CHOICES = (('English', 'English'), ('Japanese', 'Japanese'), ('chinese'
                     ('Hindi', 'Hindi'), ('italian', 'italian'), ('malaysian', 'malaysian'), ('dutch', 'dutch'),
                     ('portukal ', 'portukal '), ('russian', 'russian'), ('thai', 'thai'), ('turkish', 'turkish'))
 
+
 # Create your models here.
 class Project(models.Model):
     image = models.ImageField(upload_to='project/', null=False)
     l_title = models.CharField(choices=LANGUAGE_CHOICES, max_length=20, null=False, blank=False)
     description = models.CharField(max_length=200, null=True)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.l_title
